39c0ac4eeff06aee844b7dcfaf0ef886dd4a8c85
Medo <mohammed.yuossry@gmail.com>
2023-12-20 03:03:52 +0200

    delete older files

--- app/createnewuser/layout.js
+++
@@ -1,8 +0,0 @@
-export const metadata = {
-  title: "sign up",
-  description:
-    "this page for creating new users and posting data to the api",
-};
-const layout = ({ children }) => children;
-
-export default layout;
