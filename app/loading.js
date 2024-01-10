6da8a40b715f937d3bdbb71450f4901680814db6
Medo <mohammed.yuossry@gmail.com>
2024-01-10 09:34:31 +0200

    fix

--- app/loading.js
+++ app/loading.js
@@ -2,7 +2,7 @@ import styles from "./loading.module.css";
 
 const Loading = () => {
   return (
-    <div class={styles.lds_ellipsis}>
+    <div className={styles.lds_ellipsis}>
       <div></div>
       <div></div>
       <div></div>
