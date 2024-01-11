ac6014bdf36cb9b06b14b93f6ca4d14c702929fb
Medo <mohammed.yuossry@gmail.com>
2024-01-11 14:29:30 +0200

    login page

--- app/(auth)/login/page.js
+++ app/(auth)/login/page.js
@@ -1,4 +1,12 @@
-const Login = () => {
-  return <div>Login Page</div>;
+import { handleGitLogin } from "@/lib/actions";
+import styles from "./login.module.css";
+
+const LoginPage = () => {
+  return (
+    <form action={handleGitLogin}>
+      <button className={styles.github}>Login with Github</button>
+    </form>
+  );
 };
-export default Login;
+
+export default LoginPage;
